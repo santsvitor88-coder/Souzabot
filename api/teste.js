71698434a15cd7237f481f5a22ccb2f00dfe25a2
santsvitor88-coder <santsvitor88@gmail.com>
2025-10-28 13:08:28 -0300

    Delete api/teste.js

--- api/teste.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  const key = req.headers.authorization
-  if (key !== process.env.API_KEY) {
-    return res.status(403).json({ error: '❌ Chave inválida ou ausente' })
-  }
-
-  res.status(200).json({ sucesso: true, mensagem: "🔓 A chave API_KEY está funcionando!" })
-}
